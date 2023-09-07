bcea763380418f782c8f9b2dfcc30980ccd300d4
happlesful <Swislar1108@gmail.com>
2023-09-07 19:07:18 +0800

    added new components

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,4 +1,5 @@
 export { default as Navbar } from "./Navbar";
 export { default as Sidebar } from "./Sidebar";
 export { default as Scrollcard } from "./Scrollcard";
+export { default as ScrollImage } from "./ScrollImage";
 export { default as Body } from "./Body";
