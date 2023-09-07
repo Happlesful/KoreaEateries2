73a053dad7214392edd0129ff895d7d6fc57fc6e
happlesful <Swislar1108@gmail.com>
2023-09-07 19:05:32 +0800

    updated margins

--- src/pages/Restaurants.jsx
+++ src/pages/Restaurants.jsx
@@ -22,36 +22,50 @@ const Restaurants = (props) => {
     <>
       <div className="flex flex-col px-10 my-1">
         <section className="justify-center items-center pt-5">
-          <span
-            className="flex justify-center my-1 -translate-x-36"
-            id="display-restaurant"
-          >
-            Restaurants
-          </span>
-          <span className="flex flex-row flex-wrap justify-center items-center">
-            <span
-              className="flex flex-row flex-wrap justify-center items-center
-                w-[32rem]"
-            >
-              {filteredRestaurants(location, menu).map((restaurant, index) => {
-                return (
-                  <span
-                    className="flex flex-col justify-center items-center
-                            px-5 py-2"
-                    key={index}
-                  >
-                    <img
-                      src={restaurant.Image}
-                      alt={<AiOutlineLoading className="animate-spin" />}
-                      className="h-40 w-40 cursor-pointer rounded-xl hover:opacity-80"
-                      onClick={() => window.open(restaurant.Website, "_blank")}
-                    />
-                    <p>{restaurant.Name}</p>
-                  </span>
-                );
-              })}
+          {filteredRestaurants(location, menu).length === 0 ? (
+            <span className="flex flex-row justify-center items-center mt-5 text-lg">
+              No restaurants found
             </span>
-          </span>
+          ) : (
+            <>
+              <span
+                className="flex justify-center my-1 -translate-x-36"
+                id="display-restaurant"
+              >
+                Restaurants
+              </span>
+              <span className="flex flex-row flex-wrap justify-center items-center">
+                <span
+                  className="flex flex-row flex-wrap justify-center items-center
+                w-[32rem]"
+                >
+                  {filteredRestaurants(location, menu).map(
+                    (restaurant, index) => {
+                      return (
+                        <span
+                          className="flex flex-col justify-center items-center
+                            px-5 py-2"
+                          key={index}
+                        >
+                          <img
+                            src={restaurant.Image}
+                            alt={<AiOutlineLoading className="animate-spin" />}
+                            className="h-40 w-40 cursor-pointer rounded-xl hover:opacity-80"
+                            onClick={() =>
+                              window.open(restaurant.Website, "_blank")
+                            }
+                          />
+                          <p className="flex flex-row flex-wrap items-center justify-center">
+                            {restaurant.Name}
+                          </p>
+                        </span>
+                      );
+                    }
+                  )}
+                </span>
+              </span>
+            </>
+          )}
         </section>
       </div>
     </>
