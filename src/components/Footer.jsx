a0263eda682c20433dc73561b0515f2098fee711
happlesful <Swislar1108@gmail.com>
2023-09-11 21:37:52 +0800

    inital commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { GitHub, Calendar } from "react-feather";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="flex flex-col w-screen bg-neutral-300">
+      <div className="p-5" id="content">
+        <span className="flex flex-row justify-center items-center">
+          <GitHub className="mr-1 scale-90" /> GitHub:
+          <a href="https://github.com/Happlesful" className="mx-1">
+            Happlesful
+          </a>
+        </span>
+        <span className="flex flex-row justify-center items-center">
+          <Calendar className="mr-1 scale-90" />
+          Last updated: 11.09.23
+        </span>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
