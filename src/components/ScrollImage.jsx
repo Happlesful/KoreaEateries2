e642780d696bed039496f832215df9dca3e6285a
happlesful <Swislar1108@gmail.com>
2023-09-11 21:36:39 +0800

    improved interface

--- src/components/ScrollImage.jsx
+++ src/components/ScrollImage.jsx
@@ -24,24 +24,17 @@ const ScrollImage = (Props) => {
   };
 
   return (
-    <div className="flex flex-col justify-center items-center">
+    <div className="flex flex-col justify-center items-center p-1 outline outline-gray-700">
       <div className="relative overflow-hidden">
         <div
-          className="flex flex-row w-72 h-72 items-center
+          className="flex flex-row w-60 h-60 items-center
             transition-transform duration-1000 ease-out"
-          style={{ transform: `translateX(-${imageIndex * 102}%)` }}
+          style={{ transform: `translateX(-${imageIndex * 100}%)` }}
         >
           {featuredRestaurants.map((restaurant, index) => (
             <>
-              <a
-                href={restaurant.Website}
-                target="_blank"
-                className="bg-slate-300 p-1 rounded-lg hover:opacity-80 flex flex-col"
-              >
-                {restaurant.Name}
-              </a>
               <img
-                className={`w-60 h-60 mx-4 p-1 ease-out duration-1000 relative
+                className={`w-60 h-60 ease-out duration-1000 relative
                         ${index === imageIndex ? "scale-100" : "scale-75"}`}
                 src={restaurant.Image}
                 alt="restaurant image"
@@ -51,31 +44,31 @@ const ScrollImage = (Props) => {
         </div>
         <div
           className="absolute inset-0 flex items-center p-4 justify-between
-                left-4 -right-2"
+                -left-3 -right-3"
         >
           <button
             className="p-1 rounded-3xl shadow text-grey
-            bg-white opacity-80 hover:opacity-100"
+            bg-white opacity-50 hover:opacity-90"
             onClick={prev}
           >
             <ChevronLeft />
           </button>
           <button
             className="p-1 rounded-3xl shadow text-grey
-            bg-white opacity-80 hover:opacity-100"
+            bg-white opacity-50 hover:opacity-90"
             onClick={next}
           >
             <ChevronRight />
           </button>
         </div>
-        <span className="absolute bottom-8 right-0 left-8">
+        <span className="absolute bottom-1 right-0 left-0">
           <span className="flex justify-center items-center gap-1">
             {featuredRestaurants.map((restaurants, index) => (
               <span
                 className={`rounded-3xl w-3 h-3 bg-white ${
                   index === imageIndex
-                    ? "opacity-80 scale-75"
-                    : "opacity-30 scale-50"
+                    ? "opacity-90 scale-50"
+                    : "opacity-50 scale-40"
                 }`}
                 key={index}
               />
@@ -83,11 +76,32 @@ const ScrollImage = (Props) => {
           </span>
         </span>
       </div>
-      <span>
-        <p className="flex justify-center text-xs">
-          click on the name to open the website
-        </p>
-      </span>
+      <div className="flex flex-row mt-2 text-sm">
+        <span
+          className="flex flex-col w-60 h-14 rounded-md justify-center items-center
+        bg-zinc-200 ease-in duration-200 hover:bg-zinc-300"
+        >
+          {featuredRestaurants.map((restaurant, index) => (
+            <div
+              className="flex flex-col justify-center items-center cursor-pointer 
+              transition-opacity duration-1000 ease-linear"
+              style={{
+                opacity: imageIndex === index ? 1 : 0,
+              }}
+              onClick={() => {
+                window.open(restaurant.Website);
+              }}
+            >
+              <p key={index} className="text-lg">
+                {imageIndex === index ? restaurant.Name : ""}
+              </p>
+              <p key={index + featuredRestaurants.length} className="text-xs">
+                {imageIndex === index ? restaurant.Location : ""}
+              </p>
+            </div>
+          ))}
+        </span>
+      </div>
     </div>
   );
 };
