e7b54185b7681559dfe7e8b2a77da177ccb23427
happlesful <Swislar1108@gmail.com>
2023-09-11 21:37:09 +0800

    improved useability

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -18,7 +18,10 @@ const Navbar = () => {
             <AiOutlineMenu
               className="relative end-4 scale-150 cursor-pointer"
               onClick={() => {
-                setActiveSidebar(!activeSidebar);
+                setActiveSidebar(false);
+              }}
+              onMouseEnter={() => {
+                setActiveSidebar(true);
               }}
             />
           </span>
@@ -37,13 +40,16 @@ const Navbar = () => {
             <AiOutlineMenu
               className="relative start-4 scale-150 cursor-pointer"
               onClick={() => {
-                setActiveSidebar(!activeSidebar);
+                setActiveSidebar(false);
+              }}
+              onMouseEnter={() => {
+                setActiveSidebar(true);
               }}
             />
           </span>
         </div>
         <div className="flex flex-row items-center justify-center h-0">
-          <p className="text-xs">Click on the lines to open up the menu bar</p>
+          <p className="text-xs">Hover on the lines to open up the menu bar</p>
         </div>
       </div>
     </>
